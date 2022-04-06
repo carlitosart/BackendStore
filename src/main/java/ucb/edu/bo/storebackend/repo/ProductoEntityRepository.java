42260658883d1266e0f18f197e5aa25af8898610
carlitosart <peralta.lima.carlos@gmail.com>
2022-04-05 23:29:49 -0400

    Probando busqueda por nombre

--- src/main/java/ucb/edu/bo/storebackend/repo/ProductoEntityRepository.java
+++ src/main/java/ucb/edu/bo/storebackend/repo/ProductoEntityRepository.java
@@ -1,6 +1,7 @@
 package ucb.edu.bo.storebackend.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ucb.edu.bo.storebackend.mapping.ProductoEntity;
 
@@ -12,4 +13,10 @@ public interface ProductoEntityRepository extends CrudRepository<ProductoEntity,
     boolean existsByNombre(String nombre);
 
     List<Object> getByNombre(String nombre);
+
+    @Query(value = "SELECT tienda_online.producto.*, tienda_online.disponibilidad.stock,  tienda_online.color.nombre as color, tienda_online.talla.nombre as talla  FROM tienda_online.producto  INNER JOIN tienda_online.disponibilidad  ON tienda_online.producto.id_producto = tienda_online.disponibilidad.id_producto  INNER JOIN tienda_online.color  ON tienda_online.disponibilidad.id_color  = tienda_online.color.id_color  INNER JOIN tienda_online.talla  ON tienda_online.disponibilidad.id_talla  = tienda_online.talla.id_talla  WHERE tienda_online.producto.nombre = ?1",nativeQuery = true)
+
+    //@Query(value = "SELECT * FROM tienda_online.producto where nombre = :nompro",nativeQuery = true)
+    List<Object> getNomPro(String nompro);
+
 }
