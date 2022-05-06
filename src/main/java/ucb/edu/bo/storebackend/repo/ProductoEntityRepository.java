07853d97444fa42e4d96a2ef622d961f5f535019
Joaquin Gutierrez <joaquin.gutierrez.sanjines@gmail.com>
2022-05-06 14:16:37 -0400

    Arreglando endpoint de detalles de producto

--- src/main/java/ucb/edu/bo/storebackend/repo/ProductoEntityRepository.java
+++ src/main/java/ucb/edu/bo/storebackend/repo/ProductoEntityRepository.java
@@ -21,8 +21,18 @@ public interface ProductoEntityRepository extends CrudRepository<ProductoEntity,
 
 
 
-    @Query(value = "SELECT top.id_producto idProducto,top.nombre,top.descripcion, top.precio,top.id_categoria idCategoria, tienda_online.disponibilidad.stock,  tienda_online.color.nombre as color, tienda_online.talla.nombre as talla  FROM tienda_online.producto  top INNER JOIN tienda_online.disponibilidad  ON top.id_producto = tienda_online.disponibilidad.id_producto  INNER JOIN tienda_online.color  ON tienda_online.disponibilidad.id_color  = tienda_online.color.id_color  INNER JOIN tienda_online.talla  ON tienda_online.disponibilidad.id_talla  = tienda_online.talla.id_talla  WHERE top.nombre = ?1",nativeQuery = true)
-
+        /*SELECT tienda_online.producto.*,tienda_online.categoria.nombre, tienda_online.disponibilidad.id_disponibilidad, tienda_online.disponibilidad.stock, tienda_online.color.nombre, tienda_online.talla.nombre 
+    FROM tienda_online.producto
+    INNER JOIN tienda_online.categoria
+        ON tienda_online.producto.id_categoria = tienda_online.categoria.id_categoria
+    INNER JOIN tienda_online.disponibilidad 
+        ON tienda_online.producto.id_producto = tienda_online.disponibilidad.id_producto
+            INNER JOIN tienda_online.color
+                ON tienda_online.disponibilidad.id_color = tienda_online.color.id_color
+            INNER JOIN tienda_online.talla
+                ON tienda_online.disponibilidad.id_talla = tienda_online.talla.id_talla
+    WHERE tienda_online.producto.nombre = "Calcetines VMW" */
+    @Query(value = "SELECT top.id_producto idProducto,top.nombre,top.descripcion,top.descuento,top.disponibilidad, top.precio,top.id_categoria idCategoria,tienda_online.categoria.nombre as categoria, tienda_online.disponibilidad.id_disponibilidad as idDisponibilidad, tienda_online.disponibilidad.stock,  tienda_online.color.nombre as color, tienda_online.talla.nombre as talla  FROM tienda_online.producto top INNER JOIN tienda_online.categoria ON top.id_categoria = tienda_online.categoria.id_categoria INNER JOIN tienda_online.disponibilidad  ON top.id_producto = tienda_online.disponibilidad.id_producto  INNER JOIN tienda_online.color  ON tienda_online.disponibilidad.id_color  = tienda_online.color.id_color  INNER JOIN tienda_online.talla  ON tienda_online.disponibilidad.id_talla  = tienda_online.talla.id_talla  WHERE top.nombre = ?1",nativeQuery = true)
     //@Query(value = "SELECT * FROM tienda_online.producto where nombre = :nompro",nativeQuery = true)
     List<ProductInfoInterface> getNomPro(String nompro);
     
