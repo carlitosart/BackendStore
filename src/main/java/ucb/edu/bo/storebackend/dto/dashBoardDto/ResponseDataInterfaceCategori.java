9eafa773f3c7292f9f3a3162abb9d40b75e3a907
Joaquin Gutierrez <joaquin.gutierrez.sanjines@gmail.com>
2022-06-03 04:10:49 -0400

    Arreglando endpoints para el Dashboard

---
+++ src/main/java/ucb/edu/bo/storebackend/dto/dashBoardDto/ResponseDataInterfaceCategori.java
@@ -0,0 +1,9 @@
+package ucb.edu.bo.storebackend.dto.dashBoardDto;
+
+public interface ResponseDataInterfaceCategori {
+
+    public Integer getcantidad();
+    public String getnombre();
+    public Double getprecioTotal();
+
+}
